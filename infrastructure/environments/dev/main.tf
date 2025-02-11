2e3757f0f9d27a8dd507313dbcaebf8b01f88bf7
keisuke70 <keith235670@gmail.com>
2025-02-11 02:59:11 +0000

    fix: Commentout Lambdas for now

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -1,7 +1,6 @@
 # environments/dev/main.tf
 module "networking" {
   source = "../../modules/networking"
-  # If you defined variables (like vpc_cidr) you could pass them here, otherwise defaults apply.
 }
 
 module "aurora" {
@@ -12,23 +11,23 @@ module "aurora" {
 }
 
 
-module "sqs" {
-  source = "../../modules/sqs"
-}
+# module "sqs" {
+#   source = "../../modules/sqs"
+# }
 
 
-module "lambda" {
-  source                   = "../../modules/lambda"
-  subnet_ids               = module.networking.private_subnet_ids
-  lambda_security_group_id = module.networking.lambda_security_group_id
-  db_secret_arn            = module.aurora.db_secret_arn
-  gemini_api_key           = var.gemini_api_key  
-  sqs_gemini_results_url   = module.sqs.sqs_gemini_results_url
-  sqs_gemini_results_arn   = module.sqs.sqs_gemini_results_arn
-}
+# module "lambda" {
+#   source                   = "../../modules/lambda"
+#   subnet_ids               = module.networking.private_subnet_ids
+#   lambda_security_group_id = module.networking.lambda_security_group_id
+#   db_secret_arn            = module.aurora.db_secret_arn
+#   gemini_api_key           = var.gemini_api_key  
+#   sqs_gemini_results_url   = module.sqs.sqs_gemini_results_url
+#   sqs_gemini_results_arn   = module.sqs.sqs_gemini_results_arn
+# }
 
-module "api_gateway" {
-  source = "../../modules/api-gateway"
-  parse_task_lambda_arn       = module.lambda.parse_task_handler.arn
-  generate_schedule_lambda_arn  = module.lambda.vpc_handler.arn
-}
+# module "api_gateway" {
+#   source = "../../modules/api-gateway"
+#   parse_task_lambda_arn       = module.lambda.parse_task_handler.arn
+#   generate_schedule_lambda_arn  = module.lambda.vpc_handler.arn
+# }
