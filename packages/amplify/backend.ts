314e04ed55c3681c1961436d4d0d12077ab978bd
keisuke70 <keith235670@gmail.com>
2025-02-21 10:29:04 +0000

    feat: amplify setup

--- packages/amplify/backend.ts
+++ packages/amplify/backend.ts
@@ -1,5 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
+
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
